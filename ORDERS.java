c0d1dfc5dc15554869bd525cc63f2ee95cd44961
Gaurang484 <gaurangpatel484@gmail.com>
2018-03-29 21:15:07 +0530

    Update ORDERS.java

--- ORDERS.java
+++ ORDERS.java
@@ -12,8 +12,15 @@ class ORDERS{
 			int[] rank = new int[n];
 			for(int i = 0; i < n; i++){
 				sol[i] = sc.nextInt();
-				rank[i] = i+1;
+				rank[i] = i+1;//assign rank in increasing order to the rank array
 			}
+			
+			//main logic : See how many places the soldier has to shift left
+			//take a pointer p and go to that many places to the left
+			//store rank of p in temp, then rank of p+1 is assigned to rank of p, because the soldiers
+			//shift one place to the right. The rank of current soldier then gets assigned to soldier at p.
+			//And the rank of p gets assigned to the current soldier.
+			
 			for(int i = n-1; i >= 0; i--){
 				int p = i - sol[i];//p points to current position after moving sol[i] places
 				int temp = rank[p];
