e21acd73204680ca08a82ad498ff8385a1155a4f
batman007db <dharmikbhanushali@gmail.com>
2018-09-09 23:20:32 +0530

    Update BeautifulArrays_Dharmik.cpp

--- BeautifulArrays_Dharmik.cpp
+++ BeautifulArrays_Dharmik.cpp
@@ -2,32 +2,32 @@
 using namespace std;
 
 int main() {
-    int a[100000], n, flag_0, flag_1, flag_neg1, flag_oth;
+    int a[100000], n, f_0, f_1, f_neg1, f_oth;
     int i, t;
     cin>>t;
     while(t--)
     {
-        flag_0 = flag_neg1 = flag_1 = flag_oth = 0;
+        f_0 = f_neg1 = f_1 = f_oth = 0;
         cin>>n;
         for (i = 0; i < n; i++)
         {
             cin>>a[i];
             if (a[i] == 0)
-                ++flag_0;
+                ++f_0;
             else if (a[i] == 1)
-                ++flag_1;
+                ++f_1;
             else if (a[i] == -1)
-                ++flag_neg1;
+                ++f_neg1;
             else
-                ++flag_oth;
+                ++f_oth;
         }
-        if (flag_oth > 1)
+        if (f_oth > 1)
             cout<<"no\n";
         else
         {
-            if (flag_oth && flag_neg1)
+            if (f_oth && f_neg1)
                 cout<<"no\n";
-            else if (flag_neg1 > 1 && flag_1 == 0)
+            else if (f_neg1 > 1 && f_1 == 0)
                 cout<<"no\n";
             else
                cout<<"yes\n";
